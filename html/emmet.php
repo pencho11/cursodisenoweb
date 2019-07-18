a60128e26e1f21f546cf08006d7d688ec08128c9
pencho11 <ldmlatribuna@hotmail.com>
2019-07-18 17:08:49 -0500

    Ultima sesion de mi Curso

---
+++ html/emmet.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<!-- Atajos de Emmet 
+		Agregando Meta.
+	meta:vp -->
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<!-- Atajos de Emmet 
+		Agregando Extención de css.
+	link:css -->
+	<link rel="stylesheet" href="style.css">
+	<!-- Atajos de Emmet 
+		Agregando Extención de Favicon.
+		link:favicon -->
+		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">	
+</head>
+<body>
+	<nav></nav>
+	<li><a href="http://"></a></li>
+	<!-- Atajos de Emmet 
+		Agregando varias etiquetas.
+		header>nav>al>li>*5>a:link -->
+	<header>
+		<nav>
+			<ul>
+				<li><a href="http://"></a></li>
+				<li><a href="http://"></a></li>
+				<li><a href="http://"></a></li>
+				<li><a href="http://"></a></li>
+				<li><a href="http://"></a></li>
+			</ul>
+		</nav>
+	</header>
+	<!-- Atajos de Emmet 
+		Agregando clase, id.
+		section.estaEsMiClase#ID -->
+	<section class="estaEsMiClase" id="ID"></section>
+	<!-- Atajos de Emmet
+		agreagando un link con http...
+	a:link -->
+	<a href="http://"></a>
+
+	<!-- Atajos de Emmet
+		agreagando una imagen.
+	img -->
+	<img src="dads.png" alt="">
+
+	<!-- Atajos Emmet 
+	Agregando contenido en los corchetes{}
+	section{Contenido del section} -->
+	<section>Contenido del section</section>
+	<!-- Atajos Emmet 
+	Agregando atributos dentro de la etiqueta[]
+	section[title="contenido del title"] -->
+	<section title="contenido del title"></section>
+
+	<!-- Atajos de Emmet
+		Agregando lista automatica.
+		header>nav>al>li>*5>a:link[target="blank"]{Menu #$} -->
+<header>
+	<nav>
+		<al>
+			<li>
+				<a href="http://" target="blank">Menu #1</a>
+				<a href="http://" target="blank">Menu #2</a>
+				<a href="http://" target="blank">Menu #3</a>
+				<a href="http://" target="blank">Menu #4</a>
+				<a href="http://" target="blank">Menu #5</a>
+			</li>
+		</al>
+	</nav>
+</header>
+
+
+</body>
+</html>
