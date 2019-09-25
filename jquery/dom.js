5d9676cc15c32b345ffb408d01b0302ae7a1ebde
SolucionUnoAndroidRUBEN_HERNANDEZ <ldmlatribuna@hotmail.com>
2019-09-25 00:34:33 -0500

    Avanzando con Jquery

---
+++ jquery/dom.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    // parent padre
+    // $('#tercera').parent().css({
+    //     background: '#1b3d82'
+    // });
+
+    // Parents padres
+    // $('#tercera').parents();
+
+    // Children Elementos hijo
+    // $('#padre').children().fadeOut(500);
+    // $('#padre').children('#tercera').fadeOut(500);
+    
+    // Find
+    // $('#contenedor').find('div.caja').slideUp();
+
+    // Siblings elementos hermanos
+    // $('#tercera').siblings().fadeOut(1500);
+
+    // Netx(nextAll) - Prev(prevAll) 
+    
+    // $('#tercera').next().css({
+    //     background: '#000'
+    // })
+    // $('#tercera').prev().css({
+    //     background: '#000'
+    // })
+    // $('#tercera').nextAll().css({
+    //     background: '#000'
+    // })
+    // $('#tercera').prevAll().css({
+    //     background: '#000'
+    // })
+});
---
+++ jquery/filtrar.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    // $('.caja').first().css({
+    //     background: '#000'
+    // });
+    
+    // $('.caja').last().css({
+    //     background: '#000'
+    // });
+
+    // $('.caja').eq(3).css({
+    //     background: '#000'
+    // });
+
+    // $('.caja').filter('#segunda').css({
+    //     background: '#f9a03f'
+    // });
+
+    // $('.caja').not('#segunda').css({
+    //     background: '#f9a03f'
+    // });
+});
