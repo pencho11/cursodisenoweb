bae707e0b111aa39c43d0a3560cade2baa6339f5
pencho11 <ldmlatribuna@hotmail.com>
2019-10-02 01:19:00 -0500

    Stop JQUERY

---
+++ jquery/stop.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('#ejecutar').on('click', function() {
+        $('.caja').animate({
+            marginLeft: '500px'
+        }, 5000);
+
+        $('.caja').animate({
+            marginLeft: '0px'
+        }, 5000);
+    });
+
+    $('#parar').on('click', function() {
+        // .stop(limpiar animaciones, Saltar al final)
+        $('.caja').stop(false, true);
+    });
+});
